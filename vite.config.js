b389fee42bb7d0ec335c2fbe863d2837d98f5d4d
Awan53 <mubarokg@gmail.com>
2025-10-15 09:44:59 +0700

    testing 1

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/LandingpageProfileCV/',
 
   server: {
     historyApiFallback: true, // 🔥 ini penting untuk React Router
