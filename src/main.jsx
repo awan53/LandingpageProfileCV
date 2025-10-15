e69bcb2f8f3aa3333affc2f96eaa7585e9557059
Awan53 <mubarokg@gmail.com>
2025-10-15 10:35:31 +0700

    Test 2

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,15 @@ import App from './App.jsx';
 // BARIS INI WAJIB ADA UNTUK MENGIMPOR CSS
 import './index.css'; 
 // PENTING: Gunakan BrowserRouter untuk Development Lokal
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom'; 
+import { BrowserRouter} from 'react-router-dom'; 
 import 'remixicon/fonts/remixicon.css';
 import "./i18next.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* HANYA ADA SATU ROUTER: BrowserRouter */}
-      <BrowserRouter>
-        <div className="container mx-auto px-04 ">
-        {/* App akan mengatur Navbar dan Routes di dalamnya */}
+      <BrowserRouter basename="/LandingpageProfileCV">
         <App/>
-      </div>
       </BrowserRouter>
       
     
